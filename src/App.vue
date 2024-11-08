a1e012a52bf60135298a5490cfe3509e3279b990
milton.sanchezrios88@gmail.com <milton.sanchezrios88@gmail.com>
2024-11-08 18:51:16 -0500

    Componente Header Creado

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
 </script>
 
 <template>
-  <h1 class="text-4xl text-indigo-600">pinia</h1>
+  <h1 class="text-4xl text-indigo-600 ">pinia</h1>
 </template>
 
 
---
+++ src/components/Header.vue
@@ -0,0 +1,17 @@
+<script setup>
+
+</script>
+<template>
+    <header>
+        <div class="mx-auto container px-5 py-16">
+            <form class="space-y-4">
+                <label for="nombre">
+                    Nombre o Ingredientes
+                </label>
+            </form>
+        </div>
+    </header>
+</template>
+
+
+
